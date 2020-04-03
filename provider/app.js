707e5079293ede14f8ec6ac8ff8d4b564c93531e
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-04-03 14:15:21 +1100

    Return under_maintenance error
    Initial implementation of the reserve payment endpoint. It currently
    will always just return under_maintenance to so I can start gathering
    some real world requests to make implementing the komoju-signature
    verification easier.

---
+++ provider/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post("/", (req, res) => {
+  console.log("headers", req.headers);
+  console.log("req.body", req.body);
+  res.setHeader("Content-Type", "application/json");
+  res.send(
+    JSON.stringify({
+      success: false,
+      error: { type: "under_maintenance", message: "still being built" }
+    })
+  );
+});
+
+app.listen(port, () =>
+  console.log(`Provider is running on http://localhost:${port}`)
+);
