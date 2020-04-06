7ea18aa9382eddee5fda67171f7147add03cf059
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-04-06 14:19:11 +1000

    Return order id when right mode and event type
    When the event is a payment.type event and the app is in live mode then
    return a fake order ID, the client can use to coordinate with the
    provider directly.

--- provider/app.js
+++ provider/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require("uuid");
 
 const { verifyKomojuSignature } = require("./verifier");
 
@@ -22,6 +23,26 @@ app.post("/", (req, res) => {
   if (!isRequestVerified) {
     // figure out what the expected error return value should be
     res.status(401).send(JSON.stringify({ success: false }));
+  }
+
+  const { type, mode, payment } = req.body;
+
+  if (type === "payment.create") {
+    if (mode === "live") {
+      console.log("payment ID:", payment.id);
+      console.log("payment amount: ", payment.amount);
+      console.log("payment currency", payment.currency);
+      console.log("payment description", payment.description);
+
+      // in a real app this section would probably be reserving the payment
+      // and returning the ID assigned to the order in the database
+      const pretendOrderId = uuidv4();
+
+      res.setHeader("Content-Type", "application/json");
+      res
+        .status(200)
+        .send(JSON.stringify({ success: true, orderId: pretendOrderId }));
+    }
   }
 
   res.setHeader("Content-Type", "application/json");
