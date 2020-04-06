45098e1def8c438f9cede26d5a398fe723c7f274
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-04-06 13:56:19 +1000

    Return 401 if the signature doesn't match
    If the komuju-header signature doesn't match then return a 401
    unauthorized error.

--- provider/app.js
+++ provider/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const { verifyKomojuSignature } = require("./verifier");
+
 const app = express();
 const port = 3000;
 
@@ -9,6 +11,19 @@ app.use(bodyParser.json());
 app.post("/", (req, res) => {
   console.log("headers", req.headers);
   console.log("req.body", req.body);
+
+  const isRequestVerified = verifyKomojuSignature(
+    req.headers["komoju-signature"],
+    req.body,
+    "./keys/pub.pem"
+  );
+
+  // if the verifier is false then return a 401 Unauthorized, otherwise, continue on
+  if (!isRequestVerified) {
+    // figure out what the expected error return value should be
+    res.status(401).send(JSON.stringify({ success: false }));
+  }
+
   res.setHeader("Content-Type", "application/json");
   res.status(400).send(
     JSON.stringify({
