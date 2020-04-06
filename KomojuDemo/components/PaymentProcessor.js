0d450a4f4f953d5597dc00e87d9d133676b87b74
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-04-06 16:47:24 +1000

    Add alert pop up to ask user to confirm payment

--- KomojuDemo/components/PaymentProcessor.js
+++ KomojuDemo/components/PaymentProcessor.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Text} from 'react-native';
+import {Text, Alert} from 'react-native';
 
 const PaymentProcessor = ({route}) => {
   const {paymentUrl} = route.params;
@@ -24,12 +24,36 @@ const PaymentProcessor = ({route}) => {
         }
       })
       .then(json => {
-        console.log(
-          '***** orderId',
-          JSON.parse(json.payment.payment_details.authorization_response_text)
-            .orderId,
+        const {
+          payment: {total, currency},
+        } = json;
+        const authorizationResponseText = JSON.parse(
+          json.payment.payment_details.authorization_response_text,
         );
-        // communicate with the provider directly to capture payment
+        console.log('******** json', json);
+        console.log('total', total);
+        console.log('currency', currency);
+        Alert.alert(
+          'Payment Confirmation',
+          `Do you want to spend ${total} ${currency}?`,
+          [
+            {
+              text: 'Yes',
+              onPress: () => {
+                // communicate with the provider directly to capture payment
+                console.log('say yes to the payment');
+              },
+            },
+            {
+              text: 'No',
+              onPress: () => {
+                // go back to the home screen
+                console.log('noooooooooope');
+              },
+            },
+            {cancelable: false},
+          ],
+        );
       })
       .catch(error => console.log('ERROR: ', error))
       .finally(() => setLoadingState(false));
