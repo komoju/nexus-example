38b5971deb61ab701030247aa1fb52eec26b5868
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-04-08 13:51:29 +1000

    Refactor QRScanner to be a functional component
    It was the only component that was a class (because I initially copy
    pasted from the docs). There's no reason for it to be a class and I need
    to use the useIsFocus hook so it's been changed to a functional
    component.

--- KomojuDemo/components/QRScanner.js
+++ KomojuDemo/components/QRScanner.js
@@ -1,6 +1,7 @@
 import React, {PureComponent} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {RNCamera} from 'react-native-camera';
+import {useIsFocused} from '@react-navigation/native';
 
 const QRCodeLinksToKomojuPayments = barcodeData => {
   const expression = new RegExp('https://komoju.com/s/p/.+');
@@ -8,38 +9,33 @@ const QRCodeLinksToKomojuPayments = barcodeData => {
   return expression.test(barcodeData);
 };
 
-class QRScanner extends PureComponent {
-  render() {
-    return (
-      <View style={styles.container}>
-        <RNCamera
-          ref={ref => {
-            this.camera = ref;
-          }}
-          style={styles.preview}
-          type={RNCamera.Constants.Type.back}
-          androidCameraPermissionOptions={{
-            title: 'Permission to use camera',
-            message: 'We need your permission to use your camera',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          captureAudio={false}
-          onBarCodeRead={barcode => {
-            if (QRCodeLinksToKomojuPayments(barcode.data)) {
-              this.props.navigation.navigate('PaymentProcessor', {
-                paymentUrl: barcode.data,
-              });
-            } else {
-              // TODO: Add proper error handling here
-            }
-          }}
-          barCodeTypes={[RNCamera.Constants.BarCodeType.qr]}
-        />
-      </View>
-    );
-  }
-}
+const QRScanner = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <RNCamera
+        style={styles.preview}
+        type={RNCamera.Constants.Type.back}
+        androidCameraPermissionOptions={{
+          title: 'Permission to use camera',
+          message: 'We need your permission to use your camera',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
+        captureAudio={false}
+        onBarCodeRead={barcode => {
+          if (QRCodeLinksToKomojuPayments(barcode.data)) {
+            navigation.navigate('PaymentProcessor', {
+              paymentUrl: barcode.data,
+            });
+          } else {
+            // TODO: Add proper error handling here
+          }
+        }}
+        barCodeTypes={[RNCamera.Constants.BarCodeType.qr]}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
