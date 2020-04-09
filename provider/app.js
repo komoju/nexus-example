41fde7383e977248fa60c54c5dada9b5333ad39b
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-04-09 13:22:37 +1000

    Redirect to app link when mobile landing URL is visited
    the mobile link is configured to point to /mobile/landing on the
    provider, so the provider is responsible for redirecting to the app
    link, so the url opens in the KomojuDemo app. The session query
    parameter is being URI encoded as it's is being forwarded to the mobile
    app as a sub resource, instead of a query parameter. I couldn't get the
    query parameter to be supported by the React Native app so URI encoding
    the address and decoding it in the app was the next best solution.

--- provider/app.js
+++ provider/app.js
@@ -17,6 +17,16 @@ app.post("/capture_payment/:orderId", (req, res) => {
   capturePayment(req, res);
 });
 
+// /mobile/landing?session=https://komoju.com/s/p/6l487urgjq4369szt578f07uq
+app.get("/mobile/landing", (req, res) => {
+  const { session } = req.query;
+  const encodedSessionUrl = encodeURIComponent(session);
+  const appLink = `komoju-demo://session/${encodedSessionUrl}`;
+
+  console.log("redirecting to deep link:", appLink);
+  return res.redirect(appLink);
+});
+
 app.listen(port, () =>
   console.log(`Provider is running on http://localhost:${port}`)
 );
