89f9d8493fc3daa03e7ecf984f5e911115314c31
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-04-13 15:00:07 +1000

    Rename KomojuDemo directory to client
    Renaming the directory from KomojuDemo to client, as that more
    accurately mirrors the terminology used in the integration docs. The
    project is still called KomojuDemo itself, it's just the directory name
    that has changed.

--- scripts/build-apk.sh
+++ scripts/build-apk.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env sh
-( cd KomojuDemo/android ; ./gradlew assembleRelease)
+( cd client/android ; ./gradlew assembleRelease)
 
-cp KomojuDemo/android/app/build/outputs/apk/release/app-release.apk ./
+cp client/android/app/build/outputs/apk/release/app-release.apk ./
