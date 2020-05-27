0389f27bdcb0c22cb3445d0287e40f86b87fab9a
Nigel Baillie <metreckk@gmail.com>
2020-05-27 18:09:03 +0900

    Less redundant Error layout

--- client/components/Error.js
+++ client/components/Error.js
@@ -6,26 +6,24 @@ import globalStyles from './globalStyles';
 
 const Error = ({navigation}) => (
   <View style={globalStyles.container}>
-    <View style={globalStyles.container}>
-      <Text style={globalStyles.emoji}>😿</Text>
-      <Text style={globalStyles.text}>
-        It looks like something's gone wrong. Please try again. If you're still
-        having a problem please raise an issue on the{' '}
-        <Text
-          style={{color: 'blue'}}
-          onPress={() =>
-            Linking.openURL('https://github.com/komoju/nexus-example/issues/new')
-          }>
-          Github repository
-        </Text>
+    <Text style={globalStyles.emoji}>😿</Text>
+    <Text style={globalStyles.text}>
+      It looks like something's gone wrong. Please try again. If you're still
+      having a problem please raise an issue on the{' '}
+      <Text
+        style={{color: 'blue'}}
+        onPress={() =>
+          Linking.openURL('https://github.com/komoju/nexus-example/issues/new')
+        }>
+        Github repository
       </Text>
-      <Button
-        buttonStyle={styles.button}
-        title="Go Home"
-        titleStyle={{fontSize: 24}}
-        onPress={() => navigation.navigate('Welcome')}
-      />
-    </View>
+    </Text>
+    <Button
+      buttonStyle={styles.button}
+      title="Go Home"
+      titleStyle={{fontSize: 24}}
+      onPress={() => navigation.navigate('Welcome')}
+    />
   </View>
 );
 
